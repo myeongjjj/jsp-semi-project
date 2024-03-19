92100ebd62c6364e22425df6764912bd32e39e86
myeongjjj <shyaudwn1@naver.com>
2024-03-19 10:49:32 +0900

    인터페이스

---
+++ src/main/java/service_p/BoardService.java
@@ -0,0 +1,10 @@
+package service_p;
+
+import java.net.http.HttpResponse;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public interface BoardService {
+	void execute(HttpServletRequest request, HttpServletResponse response);
+}
